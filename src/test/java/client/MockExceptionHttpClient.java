9fed93883e82ae265ffcda6ff23b0141764fe8c8
shrechak <c.shreya2204@gmail.com>
2015-08-18 08:33:50 +0530

    refactoring mock exception client to use hashmap for matching exceptions

--- src/test/java/client/MockExceptionHttpClient.java
+++ src/test/java/client/MockExceptionHttpClient.java
@@ -8,7 +8,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MockExceptionHttpClient {
 
@@ -16,29 +18,27 @@ public class MockExceptionHttpClient {
         UNAUTHORIZED,BAD_REQUEST,INDIX_API,INTERNAL_SERVER,PAYMENT_REQUIRED,TOO_MANY_REQUESTS
     }
 
-    public HttpClient getMockClient(exception_name getException) throws IOException, IndixApiException {
+    public HttpClient getMockClient(exception_name exceptionName) throws IOException, IndixApiException {
 
-        final exception_name getExc=getException;
-        HttpClient mockhttpclient = new HttpClient() {
+        final exception_name getException=exceptionName;
+
+        final Map<exception_name, IndixApiException> mapExceptions = new HashMap<exception_name, IndixApiException>()
+        {{
+                put(exception_name.BAD_REQUEST, new BadRequestException("bad request exception"));
+                put(exception_name.UNAUTHORIZED, new UnauthorizedException("unauthorized exception"));
+                put(exception_name.TOO_MANY_REQUESTS, new TooManyRequestsException("too many requests exception"));
+                put(exception_name.PAYMENT_REQUIRED, new PaymentRequiredException("payment required exception"));
+                put(exception_name.INDIX_API, new IndixApiException(999, "some unknown error code"));
+                put(exception_name.INTERNAL_SERVER, new InternalServerException("internal server exception"));
+            }};
+
+        return new HttpClient() {
 
             public String GET(URI uri) throws IOException, IndixApiException {
 
                 //throw corresponding exception as per the situation
                 //
-                if(getExc.equals(exception_name.BAD_REQUEST))
-                    throw new BadRequestException("bad request exception");
-                else if(getExc.equals(exception_name.UNAUTHORIZED))
-                    throw new UnauthorizedException("unauthorized exception");
-                else if(getExc.equals(exception_name.TOO_MANY_REQUESTS))
-                    throw new TooManyRequestsException("too many requests exception");
-                else if(getExc.equals(exception_name.PAYMENT_REQUIRED))
-                    throw new PaymentRequiredException("payment required exception");
-                else if(getExc.equals(exception_name.INDIX_API))
-                    throw new IndixApiException(999, "some unknown error code");
-                else if(getExc.equals(exception_name.INTERNAL_SERVER))
-                    throw new InternalServerException("internal server exception");
-
-                return null;
+                 throw mapExceptions.get(getException);
             }
 
             public InputStream GETStream(URI uri) throws IOException, IndixApiException {
@@ -57,6 +57,5 @@ public class MockExceptionHttpClient {
 
             }
         };
-        return mockhttpclient;
     }
 }
