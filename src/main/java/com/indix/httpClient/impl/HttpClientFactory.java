bfeab5cce2c3cefaf8cbda0c82478a4438709448
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-09-22 12:37:58 +0530

    Adding constructor to HttpClientFactory for ClosableHttpClient

--- src/main/java/com/indix/httpClient/impl/HttpClientFactory.java
+++ src/main/java/com/indix/httpClient/impl/HttpClientFactory.java
@@ -1,6 +1,7 @@
 package com.indix.httpClient.impl;
 
 import com.indix.httpClient.HttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
 
 /**
  * Instantiates http client instances
@@ -9,5 +10,9 @@ public class HttpClientFactory {
 
     public static HttpClient newHttpClient() {
         return new HttpClientImpl();
+    }
+
+    public static HttpClient newHttpClient(CloseableHttpClient httpClient) {
+        return new HttpClientImpl(httpClient);
     }
 }
