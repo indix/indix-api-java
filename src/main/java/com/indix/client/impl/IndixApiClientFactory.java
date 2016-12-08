8897f50fe06b2442af8b49d0e702e036cd37f9ba
Raja <bethleha@gmail.com>
2016-12-08 16:39:00 +0530

    Removing appId dependency with api-client

--- src/main/java/com/indix/client/impl/IndixApiClientFactory.java
+++ src/main/java/com/indix/client/impl/IndixApiClientFactory.java
@@ -9,35 +9,32 @@ import com.indix.httpClient.HttpClient;
 public class IndixApiClientFactory {
 
     /**
-     * @param appId application id
      * @param appKey application key
      * @return {@link IndixApiClient}
      */
-    public static IndixApiClient newIndixApiClient(String appId, String appKey) {
-        return new IndixApiClientImpl(appId, appKey);
+    public static IndixApiClient newIndixApiClient(String appKey) {
+        return new IndixApiClientImpl(appKey);
     }
 
     /**
      * Used for mocking http client for testing purpose.
-     * @param appId application id
      * @param appKey application key
      * @param httpClient mock http client
      * @return {@link IndixApiClient}
      */
-    public static IndixApiClient newIndixApiClient(String appId, String appKey, HttpClient httpClient) {
-        return new IndixApiClientImpl(appId, appKey, httpClient);
+    public static IndixApiClient newIndixApiClient(String appKey, HttpClient httpClient) {
+        return new IndixApiClientImpl(appKey, httpClient);
     }
 
 
     /**
      * Used for setting server scheme and host for testing purpose.
-     * @param appId application id
      * @param appKey application key
      * @param scheme http scheme
      * @param host api host to be queried
      * @return {@link IndixApiClient}
      */
-    public static IndixApiClient newIndixApiClient(String appId, String appKey, String scheme, String host) {
-        return new IndixApiClientImpl(appId, appKey, scheme, host);
+    public static IndixApiClient newIndixApiClient(String appKey, String scheme, String host) {
+        return new IndixApiClientImpl(appKey, scheme, host);
     }
 }
